def5ca61321d54bf30ec3b3e8156513e8ff4e80a
nahidsultan19 <nahidsultan38@gmail.com>
2022-02-12 02:56:17 +0600

    tailwind and js project setup.

---
+++ main.js
@@ -0,0 +1,9 @@
+// handle deposit input 
+
+const deposit = document.getElementById('deposit-button');
+
+deposit.addEventListener('click', function () {
+    // console.log('button clicked..')
+    const depostInput = document.getElementById('deposit-input');
+    console.log(depostInput.value);
+});
